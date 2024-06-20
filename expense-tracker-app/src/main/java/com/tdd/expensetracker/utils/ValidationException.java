2e0b201d2603bdb4494d06a8b76b2bc4a094a623
smfasihaly <smfasihali@gmail.com>
2024-06-21 01:55:52 +0200

    Mutation Testing

---
+++ expense-tracker-app/src/main/java/com/tdd/expensetracker/utils/ValidationException.java
@@ -0,0 +1,9 @@
+package com.tdd.expensetracker.utils;
+
+public class ValidationException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ValidationException(String message) {
+        super(message);
+    }
+}
