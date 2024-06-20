2830118a02522ab5f2eb02856f99f9449d924d39
smfasihaly <smfasihali@gmail.com>
2024-06-20 16:54:51 +0200

    Expense Controller Add logic of assigning Category when new expense

--- expense-tracker-app/src/main/java/com/tdd/expensetracker/controller/ExpenseController.java
+++ expense-tracker-app/src/main/java/com/tdd/expensetracker/controller/ExpenseController.java
@@ -3,7 +3,9 @@ package com.tdd.expensetracker.controller;
 import java.awt.image.RescaleOp;
 import java.util.List;
 
+import com.tdd.expensetracker.model.Category;
 import com.tdd.expensetracker.model.Expense;
+import com.tdd.expensetracker.repository.CategoryRepository;
 import com.tdd.expensetracker.repository.ExpenseRepository;
 import com.tdd.expensetracker.view.ExpenseView;
 
@@ -11,50 +13,71 @@ public class ExpenseController {
 
 	private ExpenseView expenseView;
 	private ExpenseRepository expenseRepository;
+	private CategoryRepository categoryRepository;
 
 	public ExpenseController(ExpenseView expenseView, ExpenseRepository expenseRepository) {
+
 		this.expenseView = expenseView;
 		this.expenseRepository = expenseRepository;
 	}
 
 	public void allExpense() {
-		expenseView.showAllExpense(expenseRepository.findAll());
 
+		expenseView.showAllExpense(expenseRepository.findAll());
 	}
 
 	public void newExpense(Expense expense) {
 
 		Expense existingExpense = expenseRepository.findById(expense.getId());
+
 		if (existingExpense != null) {
-			this.expenseView.showError("Already existing expense with id " + expense.getId(), existingExpense);
+			expenseView.showError("Already existing expense with id " + expense.getId(), existingExpense);
 			return;
 		}
+		
+		
+		String newExpenseCategoryId = (expense.getCategory()).getId();
+		Category existingCategory = categoryRepository.findById(newExpenseCategoryId);
+		
+		if (existingCategory == null) {
+			expenseView.showError("Category does not exist with id " + newExpenseCategoryId, expense);
+			return;
+		}
+
 		expenseRepository.save(expense);
 		expenseView.expenseAdded(expense);
 
 	}
 
 	public void DeleteExpense(Expense expenseToDelete) {
+
 		Expense existingExpense = expenseRepository.findById(expenseToDelete.getId());
+
 		if (existingExpense == null) {
-			this.expenseView.showError("Expense does not exist with id "+expenseToDelete.getId(), expenseToDelete);
+			this.expenseView.showError("Expense does not exist with id " + expenseToDelete.getId(), expenseToDelete);
 			return;
 		}
-		
+
 		expenseRepository.delete(expenseToDelete.getId());
 		expenseView.expenseDeleted(expenseToDelete.getId());
 	}
 
 	public void updateExpense(Expense updatedExpense) {
+
 		Expense existingExpense = expenseRepository.findById(updatedExpense.getId());
-		if(existingExpense == null) {
-			this.expenseView.showError("Expense does not exist with id "+updatedExpense.getId(), updatedExpense);
+
+		if (existingExpense == null) {
+			this.expenseView.showError("Expense does not exist with id " + updatedExpense.getId(), updatedExpense);
 			return;
 		}
+
 		expenseRepository.update(updatedExpense);
 		expenseView.expenseUpdated(updatedExpense);
 	}
+
 	
-	
+	public void setCategoryRepository(CategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
 
 }
--- expense-tracker-app/src/test/java/com/tdd/expensetracker/controller/ExpenseControlerTest.java
+++ expense-tracker-app/src/test/java/com/tdd/expensetracker/controller/ExpenseControlerTest.java
@@ -17,12 +17,15 @@ import org.mockito.MockitoAnnotations;
 import com.tdd.expensetracker.controller.ExpenseController;
 import com.tdd.expensetracker.model.Category;
 import com.tdd.expensetracker.model.Expense;
+import com.tdd.expensetracker.repository.CategoryRepository;
 import com.tdd.expensetracker.repository.ExpenseRepository;
 import com.tdd.expensetracker.view.ExpenseView;
 
 public class ExpenseControlerTest {
 	@Mock
 	private ExpenseRepository expenseRepository;
+	@Mock
+	private CategoryRepository categoryRepository;
 	@Mock
 	private ExpenseView expenseView;
 
@@ -33,29 +36,39 @@ public class ExpenseControlerTest {
 
 	@Before
 	public void setup() {
+
 		closeable = MockitoAnnotations.openMocks(this);
+		expenseController.setCategoryRepository(categoryRepository);
 	}
 
 	@After
 	public void releaseMocks() throws Exception {
+
 		closeable.close();
 	}
 
 	@Test
 	public void testAllExpense() {
+
 		List<Expense> expense = Arrays.asList(new Expense());
 		when(expenseRepository.findAll()).thenReturn(expense);
+
 		expenseController.allExpense();
+
 		verify(expenseView).showAllExpense(expense);
 	}
 
 	@Test
 	public void testNewExpenseWhenExpenseDoesNotAlreadyExist() {
 
-		Category existingCategory = new Category("1", "name1","description1");
-		Expense expense = new Expense("1", 5000, "testExpense", LocalDate.now(),existingCategory);
+		Category existingCategory = new Category("1", "name1", "description1");
+		when(categoryRepository.findById("1")).thenReturn(existingCategory);
+
+		Expense expense = new Expense("1", 5000, "testExpense", LocalDate.now(), existingCategory);
 		when(expenseRepository.findById("1")).thenReturn(null);
+
 		expenseController.newExpense(expense);
+
 		InOrder inOrder = inOrder(expenseRepository, expenseView);
 		inOrder.verify(expenseRepository).save(expense);
 		inOrder.verify(expenseView).expenseAdded(expense);
@@ -64,22 +77,45 @@ public class ExpenseControlerTest {
 	@Test
 	public void testNewExpenseWhenExpenseAlreadyExists() {
 
-		Category existingCategory = new Category("1", "name1","description1");
-		Expense newExpense = new Expense("1", 5000, "testExpense", LocalDate.now(),existingCategory);
-		Expense existingExpense = new Expense("1", 5000, "testExpense", LocalDate.now(),existingCategory);
+		Category existingCategory = new Category("1", "name1", "description1");
+
+		Expense newExpense = new Expense("1", 5000, "testExpense", LocalDate.now(), existingCategory);
+
+		Expense existingExpense = new Expense("1", 5000, "testExpense", LocalDate.now(), existingCategory);
 		when(expenseRepository.findById("1")).thenReturn(existingExpense);
+
 		expenseController.newExpense(newExpense);
+
 		verify(expenseView).showError("Already existing expense with id 1", existingExpense);
+		verifyNoMoreInteractions(ignoreStubs(expenseRepository));
+	}
+
+	@Test
+	public void testNewExpenseWhenCategoryDoesNotExists() {
+
+		Category existingCategory = new Category("1", "name1", "description1");
+		when(categoryRepository.findById("1")).thenReturn(null);
+
+		Expense newExpense = new Expense("1", 5000, "testExpense", LocalDate.now(), existingCategory);
+		when(expenseRepository.findById("1")).thenReturn(null);
+
+		expenseController.newExpense(newExpense);
+
+		verify(expenseView).showError("Category does not exist with id 1", newExpense);
 		verifyNoMoreInteractions(ignoreStubs(expenseRepository));
 
 	}
 
 	@Test
 	public void testDeleteExpenseWhenExist() {
-		Category existingCategory = new Category("1", "name1","description1");
-		Expense expenseToDelete = new Expense("1", 5000, "testExpense", LocalDate.now(),existingCategory);
+
+		Category existingCategory = new Category("1", "name1", "description1");
+
+		Expense expenseToDelete = new Expense("1", 5000, "testExpense", LocalDate.now(), existingCategory);
 		when(expenseRepository.findById("1")).thenReturn(expenseToDelete);
+
 		expenseController.DeleteExpense(expenseToDelete);
+
 		InOrder inOrder = inOrder(expenseRepository, expenseView);
 		inOrder.verify(expenseRepository).delete("1");
 		inOrder.verify(expenseView).expenseDeleted("1");
@@ -87,21 +123,30 @@ public class ExpenseControlerTest {
 
 	@Test
 	public void testDeleteExpenseWhenDoesNotExist() {
-		Category existingCategory = new Category("1", "name1","description1");
-		Expense expenseToDelete = new Expense("1", 5000, "testExpense", LocalDate.now(),existingCategory);
+
+		Category existingCategory = new Category("1", "name1", "description1");
+
+		Expense expenseToDelete = new Expense("1", 5000, "testExpense", LocalDate.now(), existingCategory);
 		when(expenseRepository.findById("1")).thenReturn(null);
+
 		expenseController.DeleteExpense(expenseToDelete);
+
 		verify(expenseView).showError("Expense does not exist with id 1", expenseToDelete);
 		verifyNoMoreInteractions(ignoreStubs(expenseRepository));
 	}
 
 	@Test
 	public void testUpdateExpenseWhenExist() {
-		Category existingCategory = new Category("1", "name1","description1");
-		Expense updatedExpense = new Expense("1", 10000, "testExpense", LocalDate.now(),existingCategory);
-		Expense existingExpense = new Expense("1", 5000, "testExpense", LocalDate.now(),existingCategory);
+
+		Category existingCategory = new Category("1", "name1", "description1");
+
+		Expense updatedExpense = new Expense("1", 10000, "testExpense", LocalDate.now(), existingCategory);
+
+		Expense existingExpense = new Expense("1", 5000, "testExpense", LocalDate.now(), existingCategory);
 		when(expenseRepository.findById("1")).thenReturn(existingExpense);
+
 		expenseController.updateExpense(updatedExpense);
+
 		InOrder inOrder = inOrder(expenseRepository, expenseView);
 		inOrder.verify(expenseRepository).update(updatedExpense);
 		inOrder.verify(expenseView).expenseUpdated(updatedExpense);
@@ -109,13 +154,17 @@ public class ExpenseControlerTest {
 
 	@Test
 	public void testUpdateExpenseWhenDoesNotExist() {
-		Category existingCategory = new Category("1", "name1","description1");
-		Expense updatedExpense = new Expense("1", 10000, "testExpense", LocalDate.now(),existingCategory);
+
+		Category existingCategory = new Category("1", "name1", "description1");
+
+		Expense updatedExpense = new Expense("1", 10000, "testExpense", LocalDate.now(), existingCategory);
 		when(expenseRepository.findById("1")).thenReturn(null);
+
 		expenseController.updateExpense(updatedExpense);
+
 		verify(expenseView).showError("Expense does not exist with id 1", updatedExpense);
 		verifyNoMoreInteractions(ignoreStubs(expenseRepository));
 
 	}
-	
+
 }
